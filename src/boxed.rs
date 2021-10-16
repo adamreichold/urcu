8a5365be9f8351621dceb417304f15e62f0818c5
Adam Reichold <adam.reichold@t-online.de>
2021-10-16 14:52:38 +0200

    Split the RCU-protected box and the basic pointer into separate types.

---
+++ src/boxed.rs
@@ -0,0 +1,205 @@
+//! A variant of `Box<T>` protected by the userspace RCU library.
+
+use std::{
+    ffi::c_void,
+    mem::forget,
+    ops::ControlFlow,
+    ptr::{null_mut, read},
+};
+
+use crate::{call_rcu_memb, synchronize_rcu_memb, RcuHead, RcuPtr, RcuRSCS, RcuRef};
+
+/// A variant of `Box<T>` protected by the userspace RCU library.
+pub struct RcuBox<T>(RcuPtr<T>)
+where
+    T: Send;
+
+unsafe impl<T> Send for RcuBox<T> where T: Send {}
+
+unsafe impl<T> Sync for RcuBox<T> where T: Send + Sync {}
+
+impl<T> RcuBox<T>
+where
+    T: Send,
+{
+    /// Initialize an empty RCU-protected box.
+    pub fn empty() -> Self {
+        Self(RcuPtr::new(null_mut()))
+    }
+
+    /// Initialize an RCU-protected box with the given value.
+    pub fn new(val: T) -> Self {
+        let ptr = Box::into_raw(Box::new(RcuHead::new(val)));
+
+        Self(RcuPtr::new(ptr))
+    }
+
+    /// Wait for the next grace period to take full ownership of the inner value.
+    pub fn into_inner(mut self) -> Option<T> {
+        unsafe {
+            synchronize_rcu_memb();
+
+            let ptr = self.0.as_ptr();
+
+            forget(self);
+
+            if !ptr.is_null() {
+                Some(read(ptr).val)
+            } else {
+                None
+            }
+        }
+    }
+
+    /// Get shared access to the inner value with a lifetime tied to the given read side critical section.
+    pub fn read<'a>(&'a self, rscs: &'a RcuRSCS) -> RcuRef<'a, T> {
+        self.0.read(rscs)
+    }
+
+    /// Assign a new value to the RCU-protected box which will eventually become visible to readers.
+    pub fn update(&self, val: T) -> RcuBox<T> {
+        let new_ptr = Box::into_raw(Box::new(RcuHead::new(val)));
+
+        let old_ptr = self.0.update(new_ptr);
+
+        Self(RcuPtr::new(old_ptr))
+    }
+
+    /// Assign a new value to the RCU-protected box if it still matches the given reference to the current value.
+    pub fn compare_and_update<'a, R>(
+        &'a self,
+        mut curr: RcuRef<'a, T>,
+        val: T,
+        mut retry: R,
+    ) -> Result<(RcuRef<'a, T>, RcuBox<T>), RcuRef<'a, T>>
+    where
+        R: FnMut(RcuRef<'a, T>, &mut T) -> ControlFlow<()>,
+    {
+        let new_ptr = Box::into_raw(Box::new(RcuHead::new(val)));
+
+        loop {
+            match self.0.compare_and_update(curr, new_ptr) {
+                Ok((curr, old_ptr)) => {
+                    return Ok((curr, Self(RcuPtr::new(old_ptr))));
+                }
+                Err(new_curr) => {
+                    curr = new_curr;
+
+                    let val = unsafe { &mut (*new_ptr).val };
+
+                    if let ControlFlow::Break(()) = retry(curr, val) {
+                        unsafe {
+                            let _ = Box::from_raw(new_ptr);
+                        }
+
+                        return Err(curr);
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl<T> Drop for RcuBox<T>
+where
+    T: Send,
+{
+    fn drop(&mut self) {
+        let ptr = self.0.as_ptr();
+
+        if !ptr.is_null() {
+            unsafe extern "C" fn drop_later<T>(head: *mut c_void) {
+                let _ = Box::from_raw(head as *mut RcuHead<T>);
+            }
+
+            unsafe {
+                call_rcu_memb(ptr as *mut c_void, drop_later::<T>);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::sync::atomic::{AtomicUsize, Ordering};
+
+    use crossbeam_utils::thread::scope;
+    use static_assertions::assert_impl_all;
+
+    use crate::{tests::RCU, RcuThread};
+
+    #[test]
+    fn rcu_box_is_send_and_sync() {
+        assert_impl_all!(RcuBox<()>: Send, Sync);
+    }
+
+    #[test]
+    fn it_works() {
+        static DROPS: AtomicUsize = AtomicUsize::new(0);
+
+        struct CountDrops {
+            reads: AtomicUsize,
+            updates: usize,
+        }
+
+        impl Drop for CountDrops {
+            fn drop(&mut self) {
+                DROPS.fetch_add(1, Ordering::Relaxed);
+            }
+        }
+
+        let count_drops = RcuBox::new(CountDrops {
+            reads: AtomicUsize::new(0),
+            updates: 0,
+        });
+
+        const NUM_THREADS: usize = 1 << 10;
+        const NUM_ITERS: usize = 1 << 10;
+
+        scope(|scope| {
+            for _ in 0..NUM_THREADS {
+                scope.spawn(|_scope| {
+                    let rcu = RcuThread::register(&RCU);
+
+                    for _ in 0..NUM_ITERS / 2 {
+                        rcu.rscs(|rscs| {
+                            count_drops.read(rscs).reads.fetch_add(1, Ordering::Relaxed)
+                        });
+                    }
+
+                    rcu.rscs(|rscs| {
+                        let curr = count_drops.read(rscs);
+
+                        let val = CountDrops {
+                            reads: AtomicUsize::new(0),
+                            updates: curr.updates + 1,
+                        };
+
+                        let _ = count_drops
+                            .compare_and_update(curr, val, |curr, val| {
+                                val.updates = curr.updates + 1;
+
+                                ControlFlow::Continue(())
+                            })
+                            .unwrap();
+                    });
+
+                    for _ in 0..NUM_ITERS / 2 {
+                        rcu.rscs(|rscs| {
+                            count_drops.read(rscs).reads.fetch_add(1, Ordering::Relaxed)
+                        });
+                    }
+                });
+            }
+        })
+        .unwrap();
+
+        assert_eq!(count_drops.into_inner().unwrap().updates, NUM_THREADS);
+
+        RCU.barrier();
+
+        assert_eq!(DROPS.load(Ordering::Relaxed), NUM_THREADS + 1);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,15 @@
 
 //! Safe wrapper of the memb variant of the [userspace RCU library](https://github.com/urcu/userspace-rcu).
 
+pub mod boxed;
+
 use std::{
     cell::Cell,
     ffi::c_void,
     fmt,
     marker::PhantomData,
-    mem::forget,
-    ops::{ControlFlow, Deref},
-    ptr::{null_mut, read},
+    ops::Deref,
+    ptr::null_mut,
     sync::atomic::{AtomicBool, Ordering},
 };
 
@@ -18,7 +19,7 @@ use std::{ptr::read_volatile, sync::atomic::AtomicPtr};
 
 static INITIALIZED: AtomicBool = AtomicBool::new(false);
 
-/// Zero-sized token representing whether the userspace RCU library was initialized.
+/// Zero-sized token representing that the userspace RCU library was initialized.
 #[derive(Clone, Copy)]
 pub struct Rcu;
 
@@ -46,7 +47,7 @@ thread_local! {
     static REGISTERED: Cell<bool> = Cell::new(false);
 }
 
-/// Zero-sized token representing whether the current thread was registered with the userspace RCU library.
+/// Zero-sized token representing that the current thread was registered with the userspace RCU library.
 pub struct RcuThread {
     _marker: PhantomData<*mut ()>,
 }
@@ -109,8 +110,7 @@ impl Drop for RcuRSCS {
     }
 }
 
-/// A variant of `Box<T>` protected by the userspace RCU library.
-pub struct RcuBox<T>
+struct RcuPtr<T>
 where
     T: Send,
 {
@@ -120,64 +120,32 @@ where
     ptr: *mut RcuHead<T>,
 }
 
-unsafe impl<T> Send for RcuBox<T> where T: Send {}
-
-unsafe impl<T> Sync for RcuBox<T> where T: Send + Sync {}
-
-impl<T> RcuBox<T>
+impl<T> RcuPtr<T>
 where
     T: Send,
 {
-    /// Initialize an empty RCU-protected box.
-    pub fn empty() -> Self {
-        let ptr = null_mut();
-
+    fn new(ptr: *mut RcuHead<T>) -> Self {
         #[cfg(atomic_ptr)]
         let ptr = AtomicPtr::new(ptr);
 
         Self { ptr }
     }
 
-    /// Initialize an RCU-protected box with the given value.
-    pub fn new(val: T) -> Self {
-        let ptr = Box::into_raw(Box::new(RcuHead::new(val)));
-
+    fn as_ptr(&mut self) -> *mut RcuHead<T> {
         #[cfg(atomic_ptr)]
-        let ptr = AtomicPtr::new(ptr);
+        let ptr = *self.ptr.get_mut();
 
-        Self { ptr }
+        #[cfg(not(atomic_ptr))]
+        let ptr = self.ptr;
+
+        ptr
     }
 
-    /// Wait for the next grace period to take full ownership of the inner value.
-    pub fn into_inner(mut self) -> Option<T> {
-        #![cfg_attr(not(atomic_ptr), allow(unused_mut))]
-
-        unsafe {
-            synchronize_rcu_memb();
-
-            #[cfg(atomic_ptr)]
-            let ptr = *self.ptr.get_mut();
-
-            #[cfg(not(atomic_ptr))]
-            let ptr = self.ptr;
-
-            forget(self);
-
-            if !ptr.is_null() {
-                Some(read(ptr).val)
-            } else {
-                None
-            }
-        }
-    }
-
-    /// Get shared access to the inner value with a lifetime tied to a read side critical section.
-    pub fn read<'a>(&'a self, _rscs: &'a RcuRSCS) -> RcuRef<'a, T> {
+    fn read<'a>(&'a self, _rscs: &'a RcuRSCS) -> RcuRef<'a, T> {
         unsafe {
             #[cfg(atomic_ptr)]
-            let ptr = read_volatile(
-                &self.ptr as *const AtomicPtr<RcuHead<T>> as *const *const RcuHead<T>,
-            );
+            let ptr =
+                read_volatile(&self.ptr as *const AtomicPtr<RcuHead<T>> as *const *mut RcuHead<T>);
 
             #[cfg(not(atomic_ptr))]
             let ptr = rcu_dereference_sym(self.ptr as *mut c_void) as *mut RcuHead<T>;
@@ -186,10 +154,7 @@ where
         }
     }
 
-    /// Assign a new value to the RCU-protected box which will eventually become visible to readers.
-    pub fn update(&self, val: T) -> RcuBox<T> {
-        let new_ptr = Box::into_raw(Box::new(RcuHead::new(val)));
-
+    fn update(&self, new_ptr: *mut RcuHead<T>) -> *mut RcuHead<T> {
         #[cfg(atomic_ptr)]
         let old_ptr = self.ptr.swap(new_ptr, Ordering::AcqRel);
 
@@ -201,102 +166,55 @@ where
             ) as *mut RcuHead<T>
         };
 
-        #[cfg(atomic_ptr)]
-        let old_ptr = AtomicPtr::new(old_ptr);
-
-        Self { ptr: old_ptr }
+        old_ptr
     }
 
-    /// Assign a new value to the RCU-protected box if it matches the given reference to the current value.
-    pub fn compare_and_update<'a, R>(
+    fn compare_and_update<'a>(
         &'a self,
         mut curr: RcuRef<'a, T>,
-        val: T,
-        mut retry: R,
-    ) -> Result<(RcuRef<'a, T>, RcuBox<T>), RcuRef<'a, T>>
-    where
-        R: FnMut(RcuRef<'a, T>, &mut T) -> ControlFlow<()>,
-    {
-        let new_ptr = Box::into_raw(Box::new(RcuHead::new(val)));
-
-        unsafe {
-            loop {
-                #[cfg(atomic_ptr)]
-                let result = self.ptr.compare_exchange(
-                    curr.ptr as *mut RcuHead<T>,
-                    new_ptr,
-                    Ordering::AcqRel,
-                    Ordering::Acquire,
-                );
-
-                #[cfg(not(atomic_ptr))]
-                let result = {
-                    let curr_ptr = rcu_cmpxchg_pointer_sym(
-                        &self.ptr as *const *mut RcuHead<T> as *mut *mut RcuHead<T>
-                            as *mut *mut c_void,
-                        curr.ptr as *mut RcuHead<T> as *mut c_void,
-                        new_ptr as *mut c_void,
-                    ) as *mut RcuHead<T>;
-
-                    if curr_ptr == curr.ptr as *mut RcuHead<T> {
-                        Ok(curr_ptr)
-                    } else {
-                        Err(curr_ptr)
-                    }
-                };
-
-                match result {
-                    Ok(curr_ptr) => {
-                        curr.ptr = new_ptr;
-
-                        #[cfg(atomic_ptr)]
-                        let curr_ptr = AtomicPtr::new(curr_ptr);
-
-                        let prev = Self { ptr: curr_ptr };
-
-                        return Ok((curr, prev));
-                    }
-                    Err(curr_ptr) => {
-                        curr.ptr = curr_ptr;
-
-                        if let ControlFlow::Break(()) = retry(curr, &mut (*new_ptr).val) {
-                            let _ = Box::from_raw(new_ptr);
-
-                            return Err(curr);
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-impl<T> Drop for RcuBox<T>
-where
-    T: Send,
-{
-    fn drop(&mut self) {
-        unsafe extern "C" fn drop_later<T>(head: *mut c_void) {
-            let _ = Box::from_raw(head as *mut RcuHead<T>);
-        }
-
+        new_ptr: *mut RcuHead<T>,
+    ) -> Result<(RcuRef<'a, T>, *mut RcuHead<T>), RcuRef<'a, T>> {
         #[cfg(atomic_ptr)]
-        let ptr = *self.ptr.get_mut();
+        let result = self.ptr.compare_exchange(
+            curr.ptr as *mut RcuHead<T>,
+            new_ptr,
+            Ordering::AcqRel,
+            Ordering::Acquire,
+        );
 
         #[cfg(not(atomic_ptr))]
-        let ptr = self.ptr;
+        let result = unsafe {
+            let curr_ptr = rcu_cmpxchg_pointer_sym(
+                &self.ptr as *const *mut RcuHead<T> as *mut *mut RcuHead<T> as *mut *mut c_void,
+                curr.ptr as *mut RcuHead<T> as *mut c_void,
+                new_ptr as *mut c_void,
+            ) as *mut RcuHead<T>;
 
-        if !ptr.is_null() {
-            unsafe {
-                call_rcu_memb(ptr as *mut c_void, drop_later::<T>);
+            if curr_ptr == curr.ptr as *mut RcuHead<T> {
+                Ok(curr_ptr)
+            } else {
+                Err(curr_ptr)
+            }
+        };
+
+        match result {
+            Ok(curr_ptr) => {
+                curr.ptr = new_ptr;
+
+                Ok((curr, curr_ptr))
+            }
+            Err(curr_ptr) => {
+                curr.ptr = curr_ptr;
+
+                Err(curr)
             }
         }
     }
 }
 
-/// Reference to the current value of a RCU-protected box.
+/// The current value of an RCU-protected pointer.
 pub struct RcuRef<'a, T> {
-    ptr: *const RcuHead<T>,
+    ptr: *mut RcuHead<T>,
     _marker: PhantomData<&'a T>,
 }
 
@@ -315,7 +233,7 @@ impl<T> fmt::Debug for RcuRef<'_, T> {
 }
 
 impl<'a, T> RcuRef<'a, T> {
-    unsafe fn new(ptr: *const RcuHead<T>) -> Self {
+    unsafe fn new(ptr: *mut RcuHead<T>) -> Self {
         Self {
             ptr,
             _marker: PhantomData,
@@ -345,8 +263,7 @@ impl<T> Deref for RcuRef<'_, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        assert!(!self.ptr.is_null());
-        unsafe { &(*self.ptr).val }
+        self.as_ref().unwrap()
     }
 }
 
@@ -399,147 +316,25 @@ extern "C" {
 mod tests {
     use super::*;
 
-    use std::sync::atomic::AtomicUsize;
+    use lazy_static::lazy_static;
+    use static_assertions::{assert_impl_all, assert_not_impl_any};
 
-    use crossbeam_utils::thread::scope;
-
-    macro_rules! assert_impl {
-        ($ty:ty: $tr:path) => {{
-            struct Wrapper<T>(T);
-
-            trait AssertImpl {
-                fn assert();
-            }
-
-            impl<T> AssertImpl for Wrapper<T>
-            where
-                T: $tr,
-            {
-                fn assert() {}
-            }
-
-            Wrapper::<$ty>::assert();
-        }};
-    }
-
-    macro_rules! assert_not_impl {
-        ($ty:ty: $tr:path) => {{
-            struct Wrapper<T>(T);
-
-            trait AssertImpl {
-                fn assert();
-            }
-
-            impl<T> AssertImpl for Wrapper<T>
-            where
-                T: $tr,
-            {
-                fn assert() {}
-            }
-
-            trait AssertNotImpl {
-                fn assert();
-            }
-
-            impl<T> AssertNotImpl for Wrapper<T> {
-                fn assert() {}
-            }
-
-            Wrapper::<$ty>::assert();
-        }};
+    lazy_static! {
+        pub static ref RCU: Rcu = Rcu::init();
     }
 
     #[test]
     fn rcu_send_and_sync() {
-        assert_impl!(Rcu: Send);
-        assert_impl!(Rcu: Sync);
+        assert_impl_all!(Rcu: Send, Sync);
     }
 
     #[test]
     fn rcu_thread_is_neither_send_nor_sync() {
-        assert_not_impl!(RcuThread: Send);
-        assert_not_impl!(RcuThread: Sync);
+        assert_not_impl_any!(RcuThread: Send, Sync);
     }
 
     #[test]
     fn rcu_rscs_is_neither_send_nor_sync() {
-        assert_not_impl!(RcuRSCS: Send);
-        assert_not_impl!(RcuRSCS: Sync);
-    }
-
-    #[test]
-    fn rcu_box_is_send_and_sync() {
-        assert_impl!(RcuBox<()>: Send);
-        assert_impl!(RcuBox<()>: Sync);
-    }
-
-    #[test]
-    fn it_works() {
-        let rcu = Rcu::init();
-
-        static DROPS: AtomicUsize = AtomicUsize::new(0);
-
-        struct CountDrops {
-            reads: AtomicUsize,
-            updates: usize,
-        }
-
-        impl Drop for CountDrops {
-            fn drop(&mut self) {
-                DROPS.fetch_add(1, Ordering::Relaxed);
-            }
-        }
-
-        let count_drops = RcuBox::new(CountDrops {
-            reads: AtomicUsize::new(0),
-            updates: 0,
-        });
-
-        const NUM_THREADS: usize = 1 << 10;
-        const NUM_ITERS: usize = 1 << 10;
-
-        scope(|scope| {
-            for _ in 0..NUM_THREADS {
-                scope.spawn(|_scope| {
-                    let rcu = RcuThread::register(&rcu);
-
-                    for _ in 0..NUM_ITERS / 2 {
-                        rcu.rscs(|rscs| {
-                            count_drops.read(rscs).reads.fetch_add(1, Ordering::Relaxed)
-                        });
-                    }
-
-                    rcu.rscs(|rscs| {
-                        let curr = count_drops.read(rscs);
-
-                        let val = CountDrops {
-                            reads: AtomicUsize::new(0),
-                            updates: curr.updates + 1,
-                        };
-
-                        let _ = count_drops
-                            .compare_and_update(curr, val, |curr, val| {
-                                val.updates = curr.updates + 1;
-
-                                ControlFlow::Continue(())
-                            })
-                            .unwrap();
-                    });
-
-                    for _ in 0..NUM_ITERS / 2 {
-                        rcu.rscs(|rscs| {
-                            count_drops.read(rscs).reads.fetch_add(1, Ordering::Relaxed)
-                        });
-                    }
-                });
-            }
-        })
-        .unwrap();
-
-        assert_eq!(count_drops.into_inner().unwrap().updates, NUM_THREADS);
-
-        rcu.barrier();
-
-        assert_eq!(DROPS.load(Ordering::Relaxed), NUM_THREADS + 1);
+        assert_not_impl_any!(RcuRSCS: Send, Sync);
     }
 }
